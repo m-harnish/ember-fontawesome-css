3cad22ebff49b5e7e3ab68a3f39586b02fa9ebe8
Marshall Harnish <m.harnish@tempered.io>
2021-03-17 15:37:46 -0700

    Fix css import

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const chalk = require('chalk');
 const fs = require('fs');
 const path = require('path');
 const Funnel = require('broccoli-funnel');
+const MergeTrees = require('broccoli-merge-trees');
 const faPath = path.dirname(require.resolve('@fortawesome/fontawesome-free/package.json'));
 
 module.exports = {
   name: require('./package').name,
 
-  treeForVendor() {
+  treeForVendor(tree) {
+    let trees = [];
+
     // Get configured fontFormats
     let fontFormats = this.fontAwesomeCssConfig.fontFormats || ['eot', 'svg', 'ttf', 'woff', 'woff2', 'otf'];
     let fontFormatsString = fontFormats.join(',');
@@ -21,47 +24,37 @@ module.exports = {
       fontFormatPattern = `*.${fontFormatsString}`;
     }
     // Funnel required font types
-    return new Funnel(faPath, {
+    let funnel = new Funnel(faPath, {
       destDir: 'fontawesome',
       include: ['css/*', `webfonts/${fontFormatPattern}`]
     });
+
+    trees.push(funnel);
+    if (tree) trees.push(tree);
+
+    return new MergeTrees(trees);
   },
 
-  included(app, parentAddon) {
+  included() {
+    this._super.included.apply(this, arguments);
 
-    while (typeof app.import !== 'function' && (app.app || app.parent)) {
-      app = app.app || app.parent;
-    }
-
-    // if app.import and parentAddon are blank, we're probably being consumed by an in-repo-addon
-    // or engine, for which the "bust through" technique above does not work.
-    if (typeof app.import !== 'function' && !parentAddon) {
-      if (app.registry && app.registry.app) {
-        app = app.registry.app;
-      }
-    }
-
-    if (!parentAddon && typeof app.import !== 'function') {
-      throw new Error('ember-fontawesome-css is being used within another addon or engine and is' +
-        ' having trouble registering itself to the parent application.');
-    }
-
+    let app = this._findHost(this);
     this.app = app;
-    this._super.included.call(this, app);
 
-    let target = (parentAddon || app);
+    let target = app;
     target.options = target.options || {};
     this.fontAwesomeCssConfig = target.options['ember-fontawesome-css'] || {};
 
-    let cssPath = 'vendor/css';
-    let fontsPath = 'vendor/webfonts';
+    let cssPath = 'vendor/fontawesome/css';
+    let fontsPath = 'vendor/fontawesome/webfonts';
     let fontsFolderPath = '/webfonts';
     let absoluteFontsPath = path.join(faPath, 'webfonts');
 
     // Import CSS directly
     let files = this.fontAwesomeCssConfig.cssFiles || ['fontawesome', 'solid', 'regular', 'brands'];
     files.forEach((file) => {
-      target.import( { development: path.join(cssPath, file+'.css'), production: path.join(cssPath, file+'.min.css') } );
+      target.import( { development: path.join(cssPath, `${file}.css`),
+                       production: path.join(cssPath, `${file}.min.css`) } );
     });
 
     // Import all files in the fonts folder when option not defined or enabled
@@ -72,7 +65,7 @@ module.exports = {
       let fontsSkipped  = []; // Bucket for fonts not imported because they already have been
 
       // Find files already imported into the fonts folder
-      (target.otherAssetPaths || []).forEach(function(asset){
+      (target.otherAssetPaths || []).forEach(function(asset) {
         if (asset.dest && asset.dest.indexOf('/webfonts') !== -1) {
           filesInFonts.push(asset.file);
         }
@@ -90,10 +83,9 @@ module.exports = {
       // Fonts that had already been imported, so we skipped..
       if (fontsSkipped.length) {
         this.ui.writeLine(chalk.red(
-          this.name + ': Fonts already imported into the "/fonts" folder [' + fontsSkipped.join(', ') +
-          '] by another addon or in your ember-cli-build.js, disable the import ' +
-          'from other locations or disable the Font Awesome import by setting ' +
-          '`includeFontFiles:false` for the "' + this.name + '" options in your ember-cli-build.js'
+          `${this.name}: Fonts already imported into the "/fonts" folder [${fontsSkipped.join(', ')}] by another addon or in your ember-cli-build.js, ` +
+          `disable the import from other locations or disable the Font Awesome import by setting \`includeFontFiles:false\` for the "${this.name}" ` +
+          `options in your ember-cli-build.js`
         ));
       }
     }
